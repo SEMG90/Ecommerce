bd2f6f13cce157b56a0f9d175d4b57fb783fe4a2
SEMG90 <mmagdy1690@gmail.com>
2024-08-28 21:35:53 +0300

    second commit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,15 +11,22 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class RegisterComponent {
   // create group controls
   register:FormGroup = new FormGroup ( {
-    name: new FormControl(null),
-    email: new FormControl(null),
-    password: new FormControl(null),
-    rePassword: new FormControl(null),
-    phone: new FormControl(null)
-  } )
+    name: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*\W)(?!.* ).{6,}$/)]),
+    repassword: new FormControl(null),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
+  }, this.passConfirm )
 
   // register submit function
   regSubmit():void{
     console.log( this.register )
   }
+
+  // password confirmation
+  passConfirm(group:AbstractControl){
+    if( group.get('password')?.value === group.get('repassword')?.value ){return null}
+    else{return {mismatch:true} }
+  }
+
 }
